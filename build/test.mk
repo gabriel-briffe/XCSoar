89197fa4bf8f8eee85a9df6b4de8d9fb00f89f29
Wolfgang Drescher <drescher.wolfgang@gmail.com>
2026-09-09 14:06:02 +0000

    Renderer/TrafficRenderer: Draw traffic as aircraft-type symbols

--- build/test.mk
+++ build/test.mk
@@ -2217,6 +2217,7 @@ RUN_MAP_WINDOW_SOURCES = \
 	$(SRC)/Renderer/TrackLineRenderer.cpp \
 	$(SRC)/Renderer/TurnBackMarkerRenderer.cpp \
 	$(SRC)/Renderer/TrafficRenderer.cpp \
+	$(SRC)/Renderer/AircraftTypeSymbolData.cpp \
 	$(SRC)/Renderer/TrailRenderer.cpp \
 	$(SRC)/Renderer/WaypointIconRenderer.cpp \
 	$(SRC)/Renderer/WaypointRenderer.cpp \
